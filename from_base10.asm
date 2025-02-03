5d16034b1705a04ee5e53621c9ddc13e8f08c632
S-Alexander-C <S-Alexander.Covi@Lwtech.Edu>
2025-02-03 15:30:26 -0800

    Update from_base10.asm

--- from_base10.asm
+++ from_base10.asm
@@ -3,6 +3,10 @@
 # Make the function visible to all assembled files.
 .globl FromBase10
 
+
+.data
+buffer: .space 33
+
 .text
 
 # Returns the representation of a base 10 source number in a specified base between 2 and 36 (inclusive).
@@ -31,13 +35,13 @@ beqz $a0, FromBase10_end_while  # If $a0 == 0, exit loop
     mflo $a0 # update $a0
     mfhi $t0 # Store remainder in $t0
     # 3b. Convert remainder to ASCII character
-    ble $t0, 9, else_to_ascii_letters  # If remainder <= 9, go to digit conversion
+    bgt $t0, 9, else_to_ascii_letters  # If remainder <= 9, go to digit conversion
         # Convert to ASCII number ('0'-'9')
         addi $t2, $t0, 48 # ASCII for numbers: '0' = 48
         j while_continue # Skip else
     else_to_ascii_letters:
-        sub $t2, $t0, 10 # Convert to letter ('A'-'Z')
-        addi $t2, $t2, 97 # ASCII for 'a' = 65
+        sub $t2, $t0, 10 # Convert to letter ('a'-'z')
+        addi $t2, $t2, 97 # ASCII for 'a' = 97
     while_continue:
     sb $t2, 0($t3) # Store ASCII character in buffer at position $t3
     addi $t3, $t3, 1 # Move pointer to next position in buffer
@@ -47,7 +51,6 @@ FromBase10_end_while:
 sb $zero, 0($t3) # Append null terminator at end of string
 # Step 4: Reverse the string
 sub $t4, $t3, $t1     # $t4 = length of string (difference between $t3 and $t1)
-la $a2, final_string   # Load the address of the final output buffer into $a2
 
 reverse_loop:
     beqz $t4, reverse_done     # If length is 0, exit loop (we're done)
