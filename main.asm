1541fd59e1456cb7f539632e58ac809de0c433d7
S-Alexander-C <S-Alexander.Covi@Lwtech.Edu>
2025-02-05 18:28:25 -0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,50 +1,38 @@
 # Main program that converts between 2 bases (milestone 3).
-#
-# TODO: implement the following.
-#   1. Read the next source base and source number from in_bases and in_numbers respectively.
-#   2. Print them.
-#   3. Invoke ToBase10 to convert the source number to an intermediate base 10 number
-#   4. Invoke FromBase10 to convert the intermediate base 10 number to the target base
-#   5. Print the target number and target base.
-#   6. Repeat until the source base is < 0.
-
 .globl strlen_loop
 
 .data
 # TODO: Add as many additional cases as you see fit.
-in_bases: .word 2 16 -1
-in_numbers: .asciiz "101011" "2b" ""
-out_bases: .word 16 2 -1
-out_numbers: .asciiz "2b" "00101011" ""
-
+in_bases: .word 2 16 10 -1
+in_numbers: .asciiz "101011" "2b" "64" ""
+out_bases: .word 16 2 8 -1
 result: .space 33  # Stores the actual output of the program
 
 # Messages
-input_message: .asciiz "The number: "
+input_message: .asciiz "\n\nThe number: "
 base_message: .asciiz "\nIn base: "
 output_message: .asciiz "\nEquals the number: "
 error: .asciiz "\nA function failed\n"
-fail: .asciiz "\nA test failed\n"
-success: .asciiz "\nProgram ran successfully\n"
+success: .asciiz "\n\nProgram ran successfully\n"
 
 
 .text
-
-# $s0 stores the pointer of the current base--both in and out
-# $s1 stores the pointer of the current number--both in and out
-
+# $s0 stores the pointer of the current in_base and out_base
+# $s1 stores the pointer of the current in_number
+# Don't ask what happened to $s2 through $s4
 # $s5 stores the current value of in_base
 # $s6 stores the current value of in_number
 # $s7 stores the current value of out_base
+# $t9 stores the current character in the string for the strlen_loop procedure
 
-# $t9 stores the current character in the string
-
+# Initialize starting values
 Initialize:
 li $s0, 0  # 4 * index of test case
 li $s1, 0  # offset into in_numbers asciiz for test case
+li $v0, 0  # Initialize return value to 0 as a precaution
 
-ReadInputs:
 # Read next in_base and in_number
+ReadInputs:
 lw $s5, in_bases($s0)  # a0: base of source number in test case
 bltz $s5, Success  # no more test cases, so terminate with success
 la $s6, in_numbers($s1)  # a1: address of source number string in test case
@@ -97,14 +85,16 @@ li $v0, 1
 move $a0, $s7
 syscall
 
-# INCREMENT POINTERS AND COUNTERS AND LOOP UNTIL ALL TEST CASES ARE RUN
-j Success
+# Get length of string to increment pointer
+la $a0, in_numbers($s0)
+jal strlen_loop
 
-# THE FOLLOWING CODE IS FOR TESTING THE PROGRAM'S OUTPUTS
-# REMOVE IT WHEN PROJECT IS FINISHED
-TestCase:
-lbu $s2, out_numbers($s1)
+add $s1, $s1, $v0 # Increment pointer for in_numbers
+addi $s0, $s0, 4 # Increment pointer for in_bases and out_bases
 
+j ReadInputs # Loop to next input
+
+# Loop that gets the length of a string
 strlen_loop:
     lb $t9, 0($a0)             # load the next character into t2
     beqz $t9, exit_strlen      # check for the null character
@@ -114,16 +104,9 @@ strlen_loop:
 exit_strlen: 
 jr $ra
 
-
 Error:
 li $v0, 4
 la $a0, error
-syscall
-j Terminate
-
-Failure: # Remove allong with Test cases
-li $v0, 4
-la $a0, fail
 syscall
 j Terminate
 
